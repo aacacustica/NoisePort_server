066ecad1f95cfa61d89009d5166775c3e28851d3
SAnti <id00706137@usal.es>
2025-03-10 09:58:01 +0000

    listing downloaded files

--- 01_retrieve_data/retrieve_data.py
+++ 01_retrieve_data/retrieve_data.py
@@ -5,26 +5,47 @@ import re
 from logging_config import setup_logging
 from config import *
 from tqdm import tqdm
+import datetime
 
 
 
+def load_downloaded_files(downloaded_file_path):
+    """Load the set of downloaded filenames from a text file."""
+    if os.path.exists(downloaded_file_path):
+        with open(downloaded_file_path, "r") as f:
+            return {line.strip() for line in f if line.strip()}
+    return set()
 
-def download_new_files(bucket_name, home_dir, logger, processed_list='processed_files.txt'):
+
+
+def update_downloaded_files(downloaded_file_path, filename):
+    """Append a downloaded filename to the text file."""
+    with open(downloaded_file_path, "a") as f:
+        f.write(filename + "\n")
+
+
+
+def download_new_files(bucket_name, home_dir, logger, downloaded_list='downloaded_files.txt'):
     """
     Downloads new files from the S3 bucket into the folder ~/RESULTADOS.
-    Keeps track of downloaded files in the processed_files.txt file.
+    Keeps track of downloaded files in the downloaded_files.txt file.
     """
     logger.info("Initializing S3")
     logger.info(f"Home directory: {home_dir}")
+    cwd = os.getcwd()
+    logger.info(f"Current dir: {cwd}")
+    exit()
+
+
+    # ---------------------------
+    # INIZIALATIN PROCESSING FILE
+    # ---------------------------
+    downloaded_files_txt = os.path.join(cwd, "downloaded_acoustic.txt")
+    logger.info(f"Saving the downloaded file txt here --> {downloaded_files_txt}")
+    
+    downloaded_files = downloaded_files(downloaded_files_txt)
 
     s3 = boto3.client('s3')
-
-    # tracking already downloaded files
-    already_processed = set()
-    if os.path.exists(processed_list):
-        with open(processed_list, 'r') as f:
-            for line in f:
-                already_processed.add(line.strip())
 
     logger.info("\nListing files in bucket")
     response = s3.list_objects_v2(Bucket=bucket_name)
@@ -34,55 +55,57 @@ def download_new_files(bucket_name, home_dir, logger, processed_list='processed_
 
 
 
-    # downloading each file that has not been processed yet
+    # downloading each file that has not been downloaded yet
     for obj in tqdm(response['Contents']):
         key = obj['Key']
         logger.info(f"Key: {key}")
         
 
-        if key not in already_processed:
-            # last part of the key as the file name
-            file_name = key.split("/")[-1]
-            logger.info(f"Filename is --> {file_name}")
+        # last part of the key as the file name
+        file_name = key.split("/")[-1]
+        logger.info(f"Filename is --> {file_name}")
 
 
-            try:
-                dt = datetime.datetime.strptime(file_name.split('.')[0], '%Y%m%d_%H%M%S')
-                hour_folder = dt.strftime('%Y%m%d_%H')
-            except Exception as e:
-                logger.warning(f"Error parsing date from filename {file_name}: {e}")
-                hour_folder = 'unknown_hour'
-
-            # hour_folder
+        try:
+            dt = datetime.datetime.strptime(file_name.split('.')[0], '%Y%m%d_%H%M%S')
+            hour_folder = dt.strftime('%Y%m%d_%H')
+        except Exception as e:
+            logger.warning(f"Error parsing date from filename {file_name}: {e}")
+            hour_folder = 'unknown_hour'
 
 
-            # if the key contains subdirectories, mirror that structure locally
-            sub_dirs = key.split("/")[:-1]
-            sub_path = os.path.join(*sub_dirs) if sub_dirs else ""
-            logger.info(sub_path)
+
+        # if the key contains subdirectories, mirror that structure locally
+        sub_dirs = key.split("/")[:-1]
+        sub_path = os.path.join(*sub_dirs) if sub_dirs else ""
+        logger.info(sub_path)
+        
+        
+        local_sub_dir = os.path.join(home_dir, sub_path, hour_folder)
+        logger.info(f"Local subdirectory: {local_sub_dir}")
+        
+        
+        os.makedirs(local_sub_dir, exist_ok=True)
+        local_file_path = os.path.join(local_sub_dir, file_name)
+        logger.info(local_file_path)
+        
+        
+
+        #-----------------
+        #DOWNLOAD
+        #-----------------
+        logger.info(f"Downloading {key} to {local_file_path}")
+        s3.download_file(bucket_name, key, local_file_path)
+
+
+        #-----------------
+        #DELETE
+        #-----------------
+        logger.info(f"Deleting {key} from S3 bucket {bucket_name}")
+        s3.delete_object(Bucket=bucket_name, Key=key)
+
             
-            
-            # local_sub_dir = os.path.join(download_path, sub_path)
-            local_sub_dir = os.path.join(home_dir, sub_path)
-            logger.info(local_sub_dir)
-            
-            os.makedirs(local_sub_dir, exist_ok=True)
-            local_file_path = os.path.join(local_sub_dir, file_name)
-            logger.info(local_file_path)
-            
-            
-            
-            logger.info(f"Downloading {key} to {local_file_path}")
-            s3.download_file(bucket_name, key, local_file_path)
-            
 
-
-            # mark file as processed
-            with open(processed_list, 'a') as f:
-                f.write(local_file_path + "\n")
-
-        else:
-            logger.warning(f"Already processed: {key}")
 
 
 def main():
